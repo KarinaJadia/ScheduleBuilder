0a61492d4bd00faa4c2332181000123bcf41b877
KarinaJadia <karina.jadia@gmail.com>
2025-01-11 10:58:48 -0500

    :construction: trying to display data in front end...

--- main.go
+++ main.go
@@ -1,24 +1,62 @@
-// main.go
 package main
 
 import (
-	"net/http"
+	"database/sql"
+	"html/template"
 	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// handler function to serve the HTML page
+type Courses struct {
+	CourseName string
+	CourseCode string
+}
+
+// handler function for HTML page
 func handler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
 func main() {
-	http.HandleFunc("/", handler) // Set handler for the root path
+	// SQLite database connection
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	// Serve static files like CSS and JavaScript
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// compile HTML template
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	
+	// handler to fetch data and display it
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT CourseName, CourseCode FROM Courses")
+		if err != nil {
+			http.Error(w, "Database query error", http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+		
+		var data []Courses
+		for rows.Next() {
+			var d Courses
+			if err := rows.Scan(&d.CourseName, &d.CourseCode); err != nil {
+				http.Error(w, "Database scan error", http.StatusInternalServerError)
+				return
+			}
+			data = append(data, d)
+		}
 
-	// Start the server
-	log.Println("Starting server on http://localhost:8080")
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, "Template execution error", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	// create server
+	log.Println("opening website on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
