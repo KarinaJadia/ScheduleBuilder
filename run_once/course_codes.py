563a5ba769c12faca8b6e18add4412c8f6541a88
KarinaJadia <karina.jadia@gmail.com>
2025-01-10 23:34:53 -0500

    :construction:

--- run_once/course_codes.py
+++ run_once/course_codes.py
@@ -4,9 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 import re # getting a little lost in the sauce with regex
 
-if __name__ == "__main__":
-    url = 'https://catalog.uconn.edu/undergraduate/courses/#coursestext' # courses url
+def courseNames(url): # gets the course names
     courses = []
+    codes = []
 
     # this section is for reading the webpage and extracting the names
     try:
@@ -33,6 +33,33 @@ if __name__ == "__main__":
             match = re.search(r'\((.*?)\)', course)
             if match:
                 code = match.group(1)
-                file.write(f'{code}\n')
+                codes.append(code)
+                file.write(f'{code}\n') # todo: save to sql file
+    
+    return codes
 
-    # print(courses)
+def codes(url): # gets the individual courses in each school
+    codes = courseNames(url)
+    for code in codes:
+        url = 'https://catalog.uconn.edu/undergraduate/courses/' + code.lower() + '/' # only works in lowercase
+        print(f'doing {code}')
+        file = open("courses.txt", "w")
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            # parse html
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # extract the individual courses
+            course_links = soup.find_all('a')  # find all anchor tags
+            for link in course_links:
+                if code in link.text.strip() and re.search(r'\d', link.text.strip()):  # only taking courses with numbers
+                    name = link.text.strip()
+                    file.write(f'{name}\n') # todo: save to sql file
+
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    codes('https://catalog.uconn.edu/undergraduate/courses/#coursestext')
