76cfce77122a4c8d79d50fa4f525828f7fcbeda6
KarinaJadia <karina.jadia@gmail.com>
2025-01-11 23:40:09 -0500

    :art: comments to explain

--- courses.sql
+++ courses.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Courses; -- records all courses for first dropdown
 CREATE TABLE Courses (
     CourseName varchar(255), -- the course name, eg Accounting
     CourseCode varchar(255), -- the course code, eg ACCT
     PRIMARY KEY (CourseCode) -- makes sure each course name is unique
 );
 
-DROP TABLE IF EXISTS Classes;
+DROP TABLE IF EXISTS Classes; -- records all classes for second dropdown
 CREATE TABLE Classes (
     ClassNum varchar(255), -- class name and number, eg ACCT 2001
     ClassCode varchar(255), -- class code, eg "ACCT"
@@ -13,7 +13,7 @@ CREATE TABLE Classes (
     FOREIGN KEY (ClassCode) REFERENCES Courses(CourseCode) -- links to Courses table
 );
 
-DROP TABLE IF EXISTS SelectedClasses;
+DROP TABLE IF EXISTS SelectedClasses; -- records selected classes for computation
 CREATE TABLE SelectedClasses (
     ClassNum varchar(255), -- class name and number, eg ACCT 2001
     PRIMARY KEY (ClassNum), -- makes sure each class name is unique
