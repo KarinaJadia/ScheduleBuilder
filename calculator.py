05d2186457c45d3195daafaaec2677732da57987
KarinaJadia <karina.jadia@gmail.com>
2025-01-12 16:28:55 -0500

    :construction: data parsing complete! time for actual algorithm....

--- calculator.py
+++ calculator.py
@@ -20,33 +20,58 @@ def getSelected(): # gets the selected classes from the database
 def getTimes(selected_class):
     url = "https://catalog.uconn.edu/course-search/?details&code=" + selected_class.replace(" ", "%20")
     print(url)
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome() # uconn just has to make this extra hard for me
     driver.get(url)
     driver.implicitly_wait(10)
     section_info = driver.find_element(By.XPATH, '/html/body/main/div[2]/div/div[2]/div[18]/div/div/div[1]').text
-    # print(section_info)
 
     # split the section info into lines
     section_lines = section_info.split("\n")[1:]
-    
-    # initialize an array to hold the parsed section data
-    course_sections = []
-    # print(section_lines)
 
+    # dictionary to store all the sections of a class
+    sections = {
+        "Class": selected_class,
+        "Lecture":[], # must be paired with discussion or lab
+        "Discussion":[],
+        "Lab":[],
+        "LSA":[] # means it is standalone, no need to take lab/discussion/lecture
+    }
+    
     # parses data
     for i in range(0,(len(section_lines)-1),5):
+        crn = section_lines[i]
         section = section_lines[i+1]
         type_ = section_lines[i+2]
         meets = section_lines[i+3]
         instructor = section_lines[i+4]
-        course_sections.append({
-            "Section": section,
-            "Type": type_,
-            "Meets": meets,
-            "Instructor": instructor
-        })
+
+        if type_ == 'DIS':
+            sections['Discussion'].append({
+                "Section": section,
+                "Meets": meets,
+                "Instructor": instructor
+            })
+        elif type_ == 'LEC':
+            sections['Lecture'].append({
+                "Section": section,
+                "Meets": meets,
+                "Instructor": instructor
+            })
+        elif type_ == 'LAB':
+            sections['Lab'].append({
+                "Section": section,
+                "Meets": meets,
+                "Instructor": instructor
+            })
+        else:
+            sections['LSA'].append({
+                "Section": section,
+                "Meets": meets,
+                "Instructor": instructor
+            })
     
-    print(course_sections)
+    print(sections)
+    return sections
 
 if __name__ == "__main__":
     selected_classes = getSelected()
