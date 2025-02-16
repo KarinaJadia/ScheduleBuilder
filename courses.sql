d5438af612824e525188bd7a17b9a86a4f978ae1
KarinaJadia <karina.jadia@gmail.com>
2025-02-16 15:51:24 -0500

    :construction: adding new tables and cleaning up

--- courses.sql
+++ courses.sql
@@ -17,5 +17,15 @@ DROP TABLE IF EXISTS SelectedClasses; -- records selected classes for computatio
 CREATE TABLE SelectedClasses (
     ClassNum varchar(255), -- class name and number, eg ACCT 2001
     PRIMARY KEY (ClassNum), -- makes sure each class name is unique
-    FOREIGN KEY (ClassNum) REFERENCES Classes(ClassNum) -- links to Classes table
+    FOREIGN KEY (ClassNum) REFERENCES Classes(ClassNum) -- links to classes table
+);
+
+DROP TABLE IF EXISTS Schedules;
+CREATE TABLE Schedules (
+    ScheduleID INT, -- each class in here should be linked to a schedule id
+    ClassNum varchar(255), -- class name and number, eg ACCT 2001
+    ClassType varchar(255), -- class type, eg lab
+    ClassSection varchar(255), -- class section, eg 101L
+    ClassTime varchar(255), -- class time, eg 'starts': ['W 680'], 'ends': ['W 730']
+    Professor varchar(255), -- professor listed for class
 );
